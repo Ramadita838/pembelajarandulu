31d5d39cd8fc6544fd5acdcac21111cd0840e65b
ramadita838 <ramaditaardiansyah01@gmail.com>
2024-09-04 17:26:18 +0700

    update terbarunya

--- contoh.go
+++ contoh.go
@@ -1,5 +1,5 @@
 package gobelajargolang
 
-func Sapa() string {
-	return "hay semua perkenalkan nama saya ramadita ardiansyah"
+func Sapa(nama string) string {
+	return "hay semua perkenalkan nama saya ramadita ardiansyah" + nama
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/Ramadita838/pembelajarandulu
+module github.com/Ramadita838/pembelajarandulu/v2
 
 go 1.22.4
